67dc7afaaa3fb07f6cf0e6b153b9116ac2d80ca7
naraeseo <naraeseoez@gmail.com>
2022-09-13 18:05:29 +0900

    section4_76 컨테이너 나누기

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,8 +1,8 @@
 import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import { useState } from 'react';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -15,20 +15,6 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  // 컨텐츠는 기본적으로 내용이 없고, 검색된 내용이 있으면 아래의 if문 내용 출력하기
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-        key={expense.id}
-      />
-    ));
-  }
-
   return (
     <div>
       <Card className='expenses'>
@@ -36,7 +22,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
